88397c46d593770bbf59bf34c9b8adeb886c01ff
meij-m <57525126+meij-m@users.noreply.github.com>
2022-02-18 12:48:58 +0100

    Add Malawi

---
+++ R/Malawi.R
@@ -0,0 +1,32 @@
+#' Contact patterns in a village in rural Malawi
+#'
+#' This dataset contains the sequence of 35117 face-to-face contacts between 86
+#' individuals living in a village in rural Malawi. The data collection was
+#' conducted between 16h December 2019 and 10th January 2020. Wearable
+#' proximity sensors recorded with a 20-second time interval every face-to-face
+#' contact between individuals that was active in the last 20 second window. A
+#' contact between two individuals is defined as the set of successive
+#' time-windows of 20 seconds during which the individuals are detected in
+#' contact. In the Malawi data object, these records are collected in a
+#' relational event sequence, with for every contact between a pair of
+#' individualsthe time of onset and its duration. The events in the sequence are
+#' undirected. Multiple events can occur at the same time point and the
+#' individuals can be in contactwith multiple other individuals at the same time.
+#'
+#' @docType data
+#'
+#' @usage data(Malawi)
+#'
+#' @format dataframe with 35117 rows and 5 columns.
+#' \describe{
+#'  \item{time}{Numeric value: time in seconds since onset of the observation period.}
+#'  \item{actor1}{Numeric value: ID of the first actor in the event.}
+#'  \item{actor2}{Numeric value: ID of the second actor in the event.}
+#'  \item{duration}{Numeric value: duration of the event in seconds.}
+#'  \item{day}{Integer: day of the month.}
+#' }
+#'
+#' @source \url{http://www.sociopatterns.org/datasets/contact-patterns-in-a-village-in-rural-malawi/}
+#'
+#' @references Ozella, L., Paolotti, D., Lichand, G. et al. Using wearable proximity sensors to characterize social contact patterns in a village of rural Malawi. EPJ Data Sci. 10, 46 (2021). \url{https://doi.org/10.1140/epjds/s13688-021-00302-w}
+"Malawi"
---
+++ data-raw/Malawi.R
@@ -0,0 +1,67 @@
+# Load data
+con <- gzcon(url("http://www.sociopatterns.org/wp-content/uploads/2021/09/tnet_malawi_pilot.csv.gz"))
+txt <- readLines(con)
+records <- read.csv(textConnection(txt), row.names = NULL)
+records <- records[,-1]
+colnames(records) <- c("time", "day", "actor1", "actor2")
+
+# Undirected edges: sort actors within rows in the records
+records[,c("actor1", "actor2")] <- t(apply(records, 1, function(x) {
+  sort(as.integer(x[c("actor1", "actor2")]))
+}))
+
+# Transform records into relational events
+# Create a risk set
+actors <- with(records, sort(unique(c(actor1, actor2))))
+riskset <- t(combn(sort(actors), 2))
+
+# Match edge ID
+records$edge <- prodlim::row.match(records[,c("actor1", "actor2")], riskset)
+
+# Transform edges
+onset <- terminus <- vector()
+onset_time <- terminus_time <- vector()
+
+for(i in 1:nrow(records)) {
+  # Did the edge occur in the last 20 seconds? If not, onset = TRUE
+  last <- records[records$time == records$time[i]-20, ]
+  onset[i] <- ifelse(records$edge[i] %in% last$edge, FALSE, TRUE)
+
+  # Does the edge occur in the next 20 seconds? If not, terminus = TRUE
+  subs <- records[records$time == records$time[i]+20, ]
+  terminus[i] <- ifelse(records$edge[i] %in% subs$edge, FALSE, TRUE)
+}
+
+for(i in 1:nrow(records)) {
+  if(onset[i]) {
+    # Set onset time if onset is true
+    onset_time[i] <- records$time[i] - 20
+  } else {
+    # Determine onset time if onset is false
+    onset_time[i] <- records$time[max(which(records$time < records$time[i] &
+        records$edge == records$edge[i] & onset))] - 20
+  }
+
+  if(terminus[i]) {
+    # Determine terminus time if terminus is true
+    terminus_time[i] <- records$time[i]
+  } else {
+    # Determine terminus time if terminus is false
+    terminus_time[i] <- records$time[min(which(records$time > records$time[i] &
+        records$edge == records$edge[i] & terminus))]
+  }
+}
+
+records$onset <- onset
+records$terminus <- terminus
+records$onset_time <- onset_time
+records$terminus_time <- terminus_time
+records$duration <- records$terminus_time - records$onset_time
+
+# Collect data
+Malawi <- records[records$onset==TRUE,
+  c("time", "actor1", "actor2", "duration", "day")]
+rownames(Malawi) <- NULL
+
+# Save
+usethis::use_data(Malawi, overwrite = TRUE)
